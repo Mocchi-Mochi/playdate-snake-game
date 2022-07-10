d5449098f00f8375df25b80678322c71e464f5fe
Lukas <dimschlukas@gmail.com>
2022-07-10 20:50:29 +0200

    add simple GAME OVER screen

--- source/main.lua
+++ source/main.lua
@@ -4,12 +4,16 @@ import "CoreLibs/graphics"
 
 local gfx <const> = playdate.graphics
 
+gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+
 local posX = 0
 local posY = 0
 local lastButtonPressed = 0 -- 0 RIGHT, 1 LEFT, 2 UP, 3 DOWN
 local velocity = 10 -- This are the FPS. Max 30
 local point = {}
 local tail = {}
+local score = 0
+local gameRunning = true
 
 local eatSound = playdate.sound.sampleplayer.new("assets/eat")
 local initState = true
@@ -18,15 +22,15 @@ function buttonPressed()
 	if (playdate.buttonIsPressed(playdate.kButtonRight) and lastButtonPressed ~= 1) then
 		lastButtonPressed = 0
 	end
-	
+
 	if (playdate.buttonIsPressed(playdate.kButtonLeft) and lastButtonPressed ~= 0) then
 		lastButtonPressed = 1
 	end
-	
+
 	if (playdate.buttonIsPressed(playdate.kButtonUp) and lastButtonPressed ~= 3) then
 		lastButtonPressed = 2
 	end
-	
+
 	if (playdate.buttonIsPressed(playdate.kButtonDown) and lastButtonPressed ~= 2) then
 		lastButtonPressed = 3
 	end
@@ -39,7 +43,30 @@ function restart()
 	velocity = 10
 	point = {}
 	tail = {}
+	score = 0
 	initState = true
+	gameRunning = true
+end
+
+function gameOver()
+	gameRunning = false
+	local str
+	gfx.setColor(gfx.kColorWhite)
+	gfx.fillRoundRect(75, 50, 250, 140, 10)
+	gfx.setColor(gfx.kColorBlack)
+	gfx.drawRoundRect(75, 50, 250, 140, 10)
+	gfx.fillRoundRect(80, 55, 240, 130, 10)
+	str = '*GAME OVER*'
+	gfx.drawText(str, 400 / 2 - gfx.getTextSize(str) / 2, 70)
+
+	gfx.drawText('Score:', 140, 100)
+	gfx.drawText(tostring(score), 240, 100)
+
+	str = 'Press A to restart'
+	gfx.drawText(str, 400 / 2 - gfx.getTextSize(str) / 2, 155)
+
+	if playdate.buttonIsPressed(playdate.kButtonA) then restart() end
+
 end
 
 function makeSnakeMove()
@@ -56,14 +83,14 @@ end
 
 function checkIfSnakeIsOutOfScreen()
 	if posX < 0 or posX >= 400 or posY < 0 or posY >= 240 then
-		restart()
+		gameOver()
 	end
 end
 
 function checkIfSnakeHasEatenItself()
 	for i = 1, #tail do
 		if posX == tail[i][1] and posY == tail[i][2] then
-			restart()
+			gameOver()
 			return
 		end
 	end
@@ -71,11 +98,15 @@ end
 
 function checkIfPointWasEaten()
 	if posX == point[1] and posY == point[2] or point[1] == nil then
-		if initState == false then eatSound:play() end
-		initState = false
+		if initState == false then
+			eatSound:play()
+			score += 1
+		else
+			initState = false
+		end
 		point[1] = math.random(39) * 10
 		point[2] = math.random(23) * 10
-		table.insert(tail, {posX, posY})
+		table.insert(tail, { posX, posY })
 		if velocity <= 30 then velocity += 1 end
 	end
 end
@@ -91,7 +122,7 @@ function printTail()
 		end
 	end
 	for i = 1, #tail do
-	  gfx.fillRect(tail[i][1], tail[i][2], 10, 10)
+		gfx.fillRect(tail[i][1], tail[i][2], 10, 10)
 	end
 end
 
@@ -99,27 +130,32 @@ function printOnScreen()
 	gfx.clear()
 	gfx.fillRect(posX, posY, 10, 10) -- This is the snake's head
 	gfx.fillRect(point[1], point[2], 10, 10) -- This is the point the snake has to eat
-	
+
 	printTail() -- This is the snake's body
-	
+
 end
 
 function playdate.update()
 	buttonPressed()
-	
-	makeSnakeMove()
-	
-	checkIfSnakeIsOutOfScreen()
-	checkIfSnakeHasEatenItself()
-	
-	checkIfPointWasEaten()
-	
-	printOnScreen()
-	
+	if gameRunning == true then
+		makeSnakeMove()
+
+		checkIfSnakeIsOutOfScreen()
+		checkIfSnakeHasEatenItself()
+
+		checkIfPointWasEaten()
+
+		printOnScreen()
+	else
+		gameOver()
+	end
 	playdate.display.setRefreshRate(velocity)
 end
 
 function playdate.leftButtonDown() leftDown = true end
+
 function playdate.rightButtonDown() rightDown = true end
+
 function playdate.upButtonDown() upDown = true end
+
 function playdate.downButtonDown() downDown = true end
